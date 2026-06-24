900e5a784e287d52f9c6f905e79e2d096f5e0a0e
Norman Breau <norman@breautek.com>
2026-06-24 20:51:51 -0300

    convert to spm

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "ByteBuffer",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "ByteBuffer",
+            targets: ["ByteBuffer"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "ByteBuffer",
+            path: "ByteBuffer",
+            exclude: [
+                "Modules",
+                "Resources"
+            ],
+            // sources: [
+            //     "Sources"
+            // ],
+            publicHeadersPath: "include",
+            // cSettings: [
+            //     // Lets the target's own .m files resolve their quote-style
+            //     // imports now that the headers live in Headers/ByteBuffer/.
+            //     .headerSearchPath("Headers/ByteBuffer")
+            // ]
+        ),
+        .testTarget(
+            name: "ByteBufferTests",
+            dependencies: ["ByteBuffer"],
+            path: "ByteBufferTests",
+            exclude: [
+                "Info.plist"
+            ]
+        )
+    ]
+)
